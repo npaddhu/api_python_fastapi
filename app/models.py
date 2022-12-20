e09bfb898e78b63502a5ba3b0974ec0eaa7e7083
Nooka <npaddhu@gmail.com>
2022-12-20 23:00:51 +0530

    updating packages path

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
-from database import Base
+from app.database import Base
 
 
 class Post(Base):
