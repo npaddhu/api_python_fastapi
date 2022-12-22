a90c0f47ab2ea7386d5fd27c0517e015cd588ce3
Nooka <npaddhu@gmail.com>
2022-12-22 12:09:28 +0530

    Removing unwanted-basics folder

--- basics/5sqlalchemy.py
+++
@@ -1,6 +0,0 @@
-'''
-SQLALCHEMY:
-    - SQLAlchemy is one of the most popular Python ORMs.
-    - It is a standalone library and has no association with FastAPI. It can be used in any other python web frameworks or any python based application.
-
-'''
